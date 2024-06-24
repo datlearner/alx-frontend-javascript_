4a0451e88a1d08341e2aeb8400b3fb5b3d509407
datlearner <kundaimuchemwa456@gmail.com>
2024-06-24 15:46:24 +0200

    modify the variables inside the function taskBlock

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     let task = false;
     let task2 = true;
-  
+
     if (trueOrFalse) {
-      /* eslint-disable no-unused-vars */
-      const task = true;
-      const task2 = false;
+        let task = true;
+        let task2 = false;
     }
-  
+
     return [task, task2];
-  }
+}
+
